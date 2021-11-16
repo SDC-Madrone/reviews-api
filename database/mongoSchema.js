a660715b60e474a5cc089b0a6d922259930cd424
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-16 17:31:01 -0600

    Complete initial mongoose schema

--- database/mongoSchema.js
+++ database/mongoSchema.js
@@ -7,22 +7,39 @@ mongoose.connect('mongodb://localhost/ratings-and-reviews', {
 
 // schema
 var reviewSchema = mongoose.Schema({
+  _id: mongoose.ObjectId,
+  product_id: Number,
+  user_id: Number,
+  helpful_count: Number,
+  not_helpful_count: Number,
+  created_at: Date,
+
+  rating: Number,
+  recommended: Boolean,
+  characteristics: [Number]
+
+  title_summary: String,
+  body: String,
+  photos: [String]
 
 });
 
 var userSchema = mongoose.Schema({
-
+  _id: mongoose.ObjectId,
+  name: String,
+  email: String
 });
 
-var userProductReviewsJoinSchema = mongoose.Schema({
-
+var characteristicSchema = mongoose.Schema({
+  _id: mongoose.ObjectId,
+  name: String,
+  value: Number
 });
+
 
 // A MODEL is a class to produce repo objects that are stored in the database,
   // you pass in the schema you created so that mongoDB knows HOW to store your data
 // each instance of a MODEL is called a DOCUMENT
-
-//
 var Reviews = mongoose.model('Review', reviewSchema);
 var Users = mongoose.model('User', userSchema);
-var ProductUserReviews = mongoose.model('ProductUserReview', userProductReviewsJoinSchema);
+var Characteristics = mongoose.model('Characteristic', characteristicSchema);
