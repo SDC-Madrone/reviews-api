18ecdba9039c07e513ac0aa74b2df4c73920cf99
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-16 17:50:39 -0600

    Complete initial sql schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,34 @@
-CREATE DATABASE ratings_and_reviews;
+CREATE DATABASE IF NOT EXISTS ratings_and_reviews;
+USE ratings_and_reviews;
 
-CREATE TABLE reviews;
+CREATE TABLE reviews (
+  id: INT AUTO_INCREMENT,
+  product_id: INT,
+  user_id: INT,
+  helpful_count: INT,
+  not_helpful_count: INT,
+  created_at: DATETIME,
+  rating: SMALLINT,
+  recommended: BOOLEAN,
+  title_summary: VARCHAR(60),
+  body: VARCHAR(1000)
+);
 
-CREATE TABLE users;
+CREATE TABLE photos (
+  id: INT AUTO_INCREMENT,
+  review_id: INT,
+  url: VARCHAR(500)
+);
 
-CREATE TABLE user-product-reviews;
+CREATE TABLE characteristics (
+  id: INT AUTO_INCREMENT,
+  name: VARCHAR(20),
+  value: SMALLINT
+);
+
+CREATE TABLE users (
+  id: INT AUTO_INCREMENT,
+  username: VARCHAR(60),
+  email: VARCHAR(60),
+);
+
