dbecbcdbaeffd6b081d86fc30756ffb50e03921c
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-19 16:39:04 -0600

    Refactor to promises

--- database/ETL.js
+++ database/ETL.js
@@ -17,49 +17,47 @@ const loadCSV = (fileName, tableName) => {
   var pathToFile = path.join(baseDirectory, fileName);
   var query;
   if (fileName === 'reviews.csv') {
-    query = "LOAD DATA LOCAL INFILE ? INTO TABLE reviews FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness);";
+    query = "LOAD DATA LOCAL INFILE ? INTO TABLE reviews FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness);";
   } else {
-    query = `LOAD DATA LOCAL INFILE ? INTO TABLE ${tableName} FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;`;
+    query = `LOAD DATA LOCAL INFILE ? INTO TABLE ${tableName} FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES;`;
   }
 
   return connection.promise().query({
       sql: query,
       values: [pathToFile],
       infileStreamFactory: () => fs.createReadStream(pathToFile)
-    },
-    (err, results, meta) => {
-      if (err) {
-        console.log(`Error loading data into table "${tableName}" from ${fileName}`);
-        connection.end();
-        throw err;
-      } else {
-        // success
-        console.log(`LOADED ${fileName}!`);
-        connection.end();
-      }
-    }
-  );
+    });
 };
 
 
-
-const initiateLoad = (csvFile, table) => {
+const loadAll = () => {
   loadCSV('reviews.csv', 'reviews')
-  .then(() => {
-
-  return loadCSV('characteristics.csv', 'characteristics')
-  })
+    .then(() => {
+      console.log(`LOADED REVIEWS`);
+      return loadCSV('characteristics.csv', 'characteristics');
+    })
+    .then(() => {
+      console.log(`LOADED CHARACTERISTICS`);
+      return loadCSV('characteristic_reviews.csv', 'characteristic_reviews');
+    })
+    .then(() => {
+      console.log('LOADED CHARACTERISTIC_REVIEWS');
+      return loadCSV('photos.csv', 'photos');
+    })
+    .then(() => {
+      console.log('LOADED PHOTOS');
+      connection.end();
+    })
+    .catch((err) => {
+      console.log('error loading csv files');
+      connection.end();
+      throw err;
+    });
 };
 
-// manually typed out the filenames and ran this module for each one, later may refactor to do all at once
-// may need to open a connection pool for 4 processes to run at once? Look into this
-initiateLoad(/*  */);
 
-// successfully loaded Nov 18th:
-
-
-  'characteristic_reviews.csv', 'characteristic_reviews'
-  'photos.csv', 'photos'
+// may be better to open a connection pool for 4 processes to run at once? Look into this
+loadAll();
 
 
 // TODO:
