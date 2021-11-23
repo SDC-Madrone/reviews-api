27ffab35a1ea4fc740a0c72c8beab855e9ff9f1b
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-22 23:09:59 -0600

    Got rid of extra nested array

--- database/mysql2Lab.js
+++ database/mysql2Lab.js
@@ -1,10 +1,10 @@
 const connection = require('./connection.js');
 
 
-var sqlQuery = `select reviews.id, JSON_ARRAYAGG(
+var sqlQuery = `select reviews.id,
   (SELECT CAST(CONCAT('[', GROUP_CONCAT(JSON_OBJECT("id", id, "url", url)), ']') AS JSON)
   FROM photos WHERE review_id = reviews.id)
-) AS results
+AS results
 FROM reviews
 WHERE product_id = 4
 GROUP BY reviews.id;`;
@@ -12,6 +12,6 @@ GROUP BY reviews.id;`;
 
 
 connection.query(sqlQuery, function(err, rows, fields) {
-  console.log('result: ', rows[1].results);
+  console.log('result 2: ', rows[1]);
   connection.end();
 });
--- database/queryLab.sql
+++ database/queryLab.sql
@@ -30,10 +30,10 @@ USE ratings_and_reviews;
 -- select product_id from reviews where id = 10;
 -- select count(photos.id), review_id from photos where review_id = 10;
 
-select reviews.id, JSON_ARRAYAGG(
+select reviews.id,
     (SELECT CAST(CONCAT('[', GROUP_CONCAT(JSON_OBJECT("id", id, "url", url)), ']') AS JSON)
     FROM photos WHERE review_id = reviews.id)
-) AS results
+AS results
 FROM reviews
 WHERE product_id = 4
 GROUP BY reviews.id;
